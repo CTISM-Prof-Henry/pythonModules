046b87ab816f50eec3b46668d1445a4333307ad0
henryzord <henry.cagnini@gmail.com>
2022-07-26 19:28:30 -0300

    improved code quality

--- atividades/kanye/songs.py
+++ atividades/kanye/songs.py
@@ -1,26 +1,35 @@
-client_access_token = 'kRNWH_GrdQsfGPOIOF4wI-hUkfhLdFry-QaNw6c0D0QIFaSf1p61Qb-vyuDlUrlS'
+"""
+https://github.com/johnwmillr/LyricsGenius
+"""
 
-import lyricsgenius as lg  # https://github.com/johnwmillr/LyricsGenius
-
-
-def get_lyrics(arr, k):  # Write lyrics of k songs by each artist in arr
-
-    genius = lg.Genius(client_access_token,  # Client access token from Genius Client API page
-                       skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"],
-                       remove_section_headers=True)
-
-    with open("lyrics.txt", "w") as some_file:  # File to write lyrics to
-
-        c = 0  # Counter
-        for name in arr:
-            try:
-                songs = (genius.search_artist(name, max_songs=k, sort='popularity')).songs
-                s = [song.lyrics for song in songs]
-                some_file.write("\n \n   <|endoftext|>   \n \n".join(s))  # Deliminator
-                c += 1
-                print(f"Songs grabbed:{len(s)}")
-            except Exception as e:
-                print(f"some exception at {name}: {c}\nMessage: {e}")
+import lyricsgenius as lg
+import json
 
 
-get_lyrics(['Logic', 'Rihanna', 'Frank Sinatra'], 3)
+def get_lyrics(artist, n_songs):
+    client_access_token = 'kRNWH_GrdQsfGPOIOF4wI-hUkfhLdFry-QaNw6c0D0QIFaSf1p61Qb-vyuDlUrlS'
+
+    genius = lg.Genius(
+        client_access_token,  # token de acesso do Genius Client API
+        skip_non_songs=True,
+        excluded_terms=["(Remix)", "(Live)"],
+        remove_section_headers=True
+    )
+
+    with open("lyrics.json", "w") as some_file:  # File to write lyrics to
+        print(f'Começando o processo de salvar músicas do {artist}...')
+        some_dict = dict()
+
+        try:
+            songs = (genius.search_artist(artist, max_songs=n_songs, sort='popularity')).songs
+            some_dict[artist] = [song.lyrics for song in songs]
+
+            print(f"Salvei {len(some_dict[artist])} músicas do {artist} no arquivo.")
+        except Exception as e:
+            print(f"Uma exceção ocorreu:\n{e}")
+
+        json.dump(some_dict, some_file, indent=2)  # escreve dicionário no arquivo json
+
+
+if __name__ == '__main__':
+    get_lyrics('Kanye West', 3)
