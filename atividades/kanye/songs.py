2a0f4f637e438779e3f490a31e322631aa6a2bf9
henryzord <henry.cagnini@gmail.com>
2022-07-26 18:54:30 -0300

    added code to download lyrics

--- atividades/kanye/songs.py
+++ atividades/kanye/songs.py
@@ -1,14 +1,26 @@
-client_id = '2uvDXgrjN4vjfsz7GzN7gyKIgz3WbAZNIUS5xsyo18oJ4w66DBftPc_nOsUdYsBP'
-client_secret = 'GflMt-C4sOeOhJUwq2u-2hhpsujtOrRLWzxnGNsQYXaexKCXn-NmCfvCVY7wsnpfxWKIue0CWblutJT4Mjffkw'
 client_access_token = 'kRNWH_GrdQsfGPOIOF4wI-hUkfhLdFry-QaNw6c0D0QIFaSf1p61Qb-vyuDlUrlS'
 
-import lyricsgenius as lg
+import lyricsgenius as lg  # https://github.com/johnwmillr/LyricsGenius
 
-# Get a real consumer key & secret from https://dev.twitter.com/apps/new
 
-def main():
-    with open("data.txt", "w") as some_file:
-        pass
+def get_lyrics(arr, k):  # Write lyrics of k songs by each artist in arr
 
-if __name__ == '__main__':
-    main()
+    genius = lg.Genius(client_access_token,  # Client access token from Genius Client API page
+                       skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"],
+                       remove_section_headers=True)
+
+    with open("lyrics.txt", "w") as some_file:  # File to write lyrics to
+
+        c = 0  # Counter
+        for name in arr:
+            try:
+                songs = (genius.search_artist(name, max_songs=k, sort='popularity')).songs
+                s = [song.lyrics for song in songs]
+                some_file.write("\n \n   <|endoftext|>   \n \n".join(s))  # Deliminator
+                c += 1
+                print(f"Songs grabbed:{len(s)}")
+            except Exception as e:
+                print(f"some exception at {name}: {c}\nMessage: {e}")
+
+
+get_lyrics(['Logic', 'Rihanna', 'Frank Sinatra'], 3)
